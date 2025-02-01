a19a15720adcf2ca01dfd82fd352dd07d80d0ca0
Gulshan <gulshankumar100g@gmail.com>
2025-02-01 15:07:06 +0530

    Updated code

--- llm.py
+++ llm.py
@@ -1,14 +1,15 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
+import os
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(find_dotenv())
 
-GEMINI_API_KEY = "AIzaSyA4KBUTVenSp6bESQHqiOAwsW0RS6QLxfM"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # Initialize the language model for tweet generation
 llm = ChatGoogleGenerativeAI(
     model="gemini-pro",
-    temperature=1,
+    temperature =1,
     google_api_key=GEMINI_API_KEY
 )
